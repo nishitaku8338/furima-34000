2cfd49c9b97b9fdf05c8887cffc6c8fe646aad07
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-09 19:39:18 +0900

    マイグレーションファイルカラムの修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_09_093551) do
     t.string "first_name", null: false
     t.string "last_name_reading", null: false
     t.string "first_name_reading", null: false
-    t.date "date", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
