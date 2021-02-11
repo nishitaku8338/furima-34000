55b7fd4670adc15e87b68ae302ad842ab1f8952c
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-11 19:08:54 +0900

    カテゴリーのプルダウンを表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :category_id, numericality: { other_than: 1 } #ジャンルの選択が「--」の時は保存できない
   
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category #ActiveHash
 end
