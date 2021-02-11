2ae341f30f20e04c28ec17e247f41e43f7c38ef0
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-11 17:58:02 +0900

    category_id バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1 } #ジャンルの選択が「--」の時は保存できない
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
