4c4bcbec77acd2003c8320bd5030773bdae954a1
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-12 22:26:22 +0900

    入力する度にイベントを発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火")
+  })
 }); 
