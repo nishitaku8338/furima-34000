adca861d8651ad752fa1478357c3164cc0e32a3d
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-12 20:41:17 +0900

    Itemモデル 単体テストコード 商品出品登録できないとき

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,5 +11,68 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
+
+    context '商品出品登録できないとき' do
+      it 'imageがないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameがないと登録できない' do
+        @item.item_name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'item_descriptionがないと登録できない' do
+        @item.item_description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+      it 'category_idが1だと登録できない' do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+      it 'item_state_idが1だと登録できない' do
+        @item.item_state_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item state Select")
+      end
+      it 'postage_idが1だと登録できない' do
+        @item.postage_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage Select")
+      end
+      it 'region_idが1だと登録できない' do
+        @item.region_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region Select")
+      end
+      it 'shipping_data_idが1だと登録できない' do
+        @item.shipping_data_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping data Select")
+      end
+      it 'priceがないと登録できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが半角数字以外だと登録できない' do
+        @item.price = "Aaあぁアァ1１一久々"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+      it 'priceが299以下だと登録できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it 'priceが9999999以上だと登録できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+    end
   end
 end
