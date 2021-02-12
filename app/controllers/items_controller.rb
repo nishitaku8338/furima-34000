81074650f5afdf0665017f1c892db0be26209486
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-12 14:39:06 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,28 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :image,
+      :item_name,
+      :item_description,
+      :category_id,
+      :item_state_id,
+      :postage_id,
+      :region_id,
+      :shipping_data_id,
+      :price
+    ).merge(user_id: current_user.id)
   end
 end
