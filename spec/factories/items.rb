b1d3395ee8a7a79b8efd598a482ce9a5c891b67c
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-12 19:06:26 +0900

    FactoryBot インスタンス生成(itemsテーブル カラム名)

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { "Aaあぁアァ1１一久々" }
+    item_description { "Aaあぁアァ1１一久々" }
+    category_id { "2" }
+    item_state_id { "2" }
+    postage_id { "2" }
+    region_id { "2" }
+    shipping_data_id { "2" }
+    price { "300" }
+
+    association :user
+
+    after(:build) do |item| # インスタンスが生成した後画像を生成
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
