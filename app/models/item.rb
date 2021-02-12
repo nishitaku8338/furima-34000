718aad499a6f9dd24ba5d22d26bcb83eea63c48c
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-12 14:12:50 +0900

    Itemモデル(商品出品項目) バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,27 @@
 class Item < ApplicationRecord
   validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 } #ジャンルの選択が「--」の時は保存できない
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :item_description, presence: true, length: { in: 7..1000 }
+  VALID_PRICEL_HALF = /\A[0-9]+\z/       # 300~9999999、半角数字以外を外す
+  validates :price, presence: true,
+    format: {with: VALID_PRICEL_HALF},
+    length: {minimum: 3, maxinum: 7},
+    numericality: { 
+    only_integer: true,                                            # 数値のみ
+    greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999  # 指定された値以上、指定された値以下
+    }
+  validates :category_id, numericality:      { other_than: 1 } #ジャンルの選択が「--」の時は保存できない
+  validates :item_state_id, numericality:    { other_than: 1 }
+  validates :postage_id, numericality:       { other_than: 1 }
+  validates :region_id, numericality:        { other_than: 1 }
+  validates :shipping_data_id, numericality: { other_than: 1 }
   
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category       #ActiveHash
   belongs_to :item_state     #ActiveHash
-  belongs_to :Postage        #ActiveHash
-  belongs_to :Region         #ActiveHash
+  belongs_to :postage        #ActiveHash
+  belongs_to :region         #ActiveHash
   belongs_to :shipping_data  #ActiveHash
 end
