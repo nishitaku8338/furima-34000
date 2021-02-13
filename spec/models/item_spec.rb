b3704062568e71b2d1076d064121b25ec759fc3d
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-13 13:24:58 +0900

    Itemモデル 単体テストコード 商品出品登録できないとき 追加記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,6 +93,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
+      it 'userが紐付いていないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        binding.pry
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
