acb0033c709c9f2d5006e6dd0cc6e9a1948c6273
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-13 12:51:40 +0900

    Itemモデル 単体テストコード 商品出品登録できないとき 追加記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,8 +58,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it 'priceが半角数字以外だと登録できない' do
-        @item.price = "Aaあぁアァ1１一久々"
+      it 'priceが全角数字だと登録できない' do
+        @item.price = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it 'priceが全角英文字だと登録できない' do
+        @item.price = "A"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+      it 'priceが半角英文字だと登録できない' do
+        @item.price = "a"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+      it 'priceがひらがなだと登録できない' do
+        @item.price = "あぁ"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number")
+      end
+      it 'priceが漢字だと登録できない' do
+        @item.price = "山田"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Half-width number")
       end
