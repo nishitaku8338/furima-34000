a3752b48e27c94432f83d1ebd4c54c11f805bdc8
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-16 16:17:18 +0900

    データをテーブルに保存する処理

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,4 +14,17 @@ class OrderAddress
     validates :phone_number, format: {with: /\A[0-9]+\z/, message: "Select"}
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)  # orderテーブル外部キー
+    Address.crate(
+      postal_code: postal_code, 
+      prefecture_id: prefecture_id, 
+      city: city, 
+      house_number: house_number, 
+      building_name: building_name, 
+      phone_number: phone_number, 
+      order_id: order.id             # addressesテーブル外部キー
+    )
+  end
 end
