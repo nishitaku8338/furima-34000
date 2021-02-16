946fabb56b9392c1ec34177b293e408751d54d1f
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-16 22:33:41 +0900

    ActiveHashを実装 (都道府県一覧を(prefecture_id)をプルダウン表示)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture       # ActiveHash
 end
