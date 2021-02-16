e2fd09de2a053d6fdd35889bbbff3cb9ad431e5a
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-16 15:25:54 +0900

    attr_accessorを用いて、保存したい複数のテーブルのカラム名すべて記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :token, 
+                :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, 
+                :user_id, :item_id
 end
