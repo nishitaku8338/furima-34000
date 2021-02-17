d0d30c1349d7c35bbdf4c3a405d3389481691634
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 15:00:34 +0900

    テストコードを実装(order_address_spec.rb)

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :order_address do
     postal_code { '123-4567' }
-    prefecture { 2 }
+    prefecture_id { 2 }
     city { '東京都' }
     house_number { '1-1' }
     building_name { '東京ハイツ' }
     phone_number { '08088883333' }
   end
+  # association :user    order_addressは自分で生成した為、アソシエーション機能を持たない
+  # association :item    order_addressは自分で生成した為、アソシエーション機能を持たない
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品購入機能の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      sleep 0.05
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@order_address).to be_valid
+    end
+    it 'postal_codeが空だと保存できないこと' do
+      @order_address.postal_code = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      @order_address.postal_code = '1234567'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'postal_codeが全角数字の場合は保存できないこと' do
+      @order_address.postal_code = '１２３-４５６７'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'postal_codeがひらがなの場合は保存できないこと' do
+      @order_address.postal_code = 'いち'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'postal_codeが漢字の場合は保存できないこと' do
+      @order_address.postal_code = '一二三'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'prefectureを1を選択すると保存できないこと' do
+      @order_address.prefecture_id = '1'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Prefecture Select")
+    end
+    it 'cityは空だと保存できないこと' do
+      @order_address.city = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("City can't be blank")
+    end
+    it 'house_numberは空だと保存できないこと' do
+      @order_address.house_number = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("House number can't be blank")
+    end
+    it 'building_nameは空でも保存できること' do
+      @order_address.building_name = ''
+      expect(@order_address).to be_valid
+    end
+    it 'phone_numberは空だと保存できないこと' do
+      @order_address.phone_number = ''
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberは全角数字だと保存できないこと' do
+      @order_address.phone_number = "０８０８８８８３３３３"
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number Input only number")
+    end
+  end
 end
+
+# テストコード実行コマンド
+# bundle exec rspec spec/models/order_address_spec.rb
