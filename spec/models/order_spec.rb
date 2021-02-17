a0b0e4783c8c61aa7a70a49c217a67ab2e1b6718
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 11:25:37 +0900

    各テストコードファイルに、テストコマンドを記述(コメントアウトで記述)

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,3 +3,6 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# テストコード実行コマンド
+# bundle exec rspec spec/models/order_address_spec.rb
