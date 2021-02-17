280d96d2b809276379a7da66be015fc5aa20d8ca
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 15:54:27 +0900

    フォーム送信時にイベントが発火するように記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {  // eは、フォームの値が送信された時をeに代入している。
+    e.preventDefault();                     // 通常のRuby on Railsにおけるフォーム送信処理はキャンセル。現状「購入」ボタンをクリックしても、サーバーサイドへリクエストは送られない。
+    console.log("フォーム送信時にイベント発火")
+  });
 };
 
 window.addEventListener("load", pay);
