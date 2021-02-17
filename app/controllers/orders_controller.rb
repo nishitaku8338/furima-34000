0d5b80516cf4fecf06eb080336fd5c9bac331c9d
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 18:22:26 +0900

    binding.pryを解除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])  # :tokenとrender action: :indexに関わる為記述が必要
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?  # バリデーションを確認
