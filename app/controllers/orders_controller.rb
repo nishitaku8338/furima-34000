4ed1b6861bf1689d726875297e1cc31bd44fc0b5
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 18:15:05 +0900

    決済処理を記述(order_addressコントローラー)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])  # :tokenとrender action: :indexに関わる為記述が必要
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?  # バリデーションを確認
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -28,5 +29,15 @@ class OrdersController < ApplicationController
       :building_name,
       :phone_number,
     ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    # 秘密鍵をPAY.JP側へ送付
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # Payjpクラスのapi_keyインスタンスに秘密鍵を代入
+    Payjp::Charge.create(
+      amount: @item.price,         # 商品価格          ※必ず記述※
+      card: order_params[:token],  # カードトークン     ※必ず記述※
+      currency: 'jpy'              # 通貨の種類(日本円) ※必ず記述※
+    )
   end
 end
