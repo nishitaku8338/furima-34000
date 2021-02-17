a7bbc674463578bed964338d1196106a71cc1c90
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 16:10:51 +0900

    公開鍵を設定(環境変数を記述)

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,8 @@
 const pay = () => {
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {  // eは、フォームの値が送信された時をeに代入している。
-    e.preventDefault();                     // 通常のRuby on Railsにおけるフォーム送信処理はキャンセル。現状「購入」ボタンをクリックしても、サーバーサイドへリクエストは送られない。
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);     // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");  // formの値全部取得する
+  form.addEventListener("submit", (e) => {              // eは、フォームの値が送信された時をeに代入している。
+    e.preventDefault();                                 // 通常のRuby on Railsにおけるフォーム送信処理はキャンセル。
     console.log("フォーム送信時にイベント発火")
   });
 };
