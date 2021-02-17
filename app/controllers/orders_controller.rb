94ad92258765fbb261915df926c463ba59eb554e
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 10:57:07 +0900

    フォームの情報を保存できるようにする(order_addressクラスの記述を一部コメントアウト)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,5 +6,26 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])  # :tokenとrender action: :indexに関わる為記述が必要
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?  # バリデーションを確認
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :building_name,
+      :phone_number,
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
