d8463866f30bf6b19abe8b6c0e2800bf7de739e3
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-17 16:26:20 +0900

    フォームの情報を取得して、変数cardに代入

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,17 @@ const pay = () => {
   const form = document.getElementById("charge-form");  // formの値全部取得する
   form.addEventListener("submit", (e) => {              // eは、フォームの値が送信された時をeに代入している。
     e.preventDefault();                                 // 通常のRuby on Railsにおけるフォーム送信処理はキャンセル。
-    console.log("フォーム送信時にイベント発火")
+    // console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");  // "charge-form"というidでフォームの情報を取得
+    const formData = new FormData(formResult);                  // FormDataオブジェクトとして生成
+
+    const card = {
+      number: formData.get("order_address[number]"),             // 生成したFormDataオブジェクトから、クレジットカードに関する情報を取得し、変数cardに代入するオブジェクトとして定義
+      cvc: formData.get("order_address[cvc]"),                   // "order_address[number]"はhtmlのname属性の値を参照する
+      exp_month: formData.get("order_address[exp_month]"),       // htmlのname属性は、ブラウザの検証を必ず使う
+      exp_year: `20${formData.get("order_address[exp_year]")}`,  // 20$は2022年の最初の20の事
+    };
   });
 };
 
