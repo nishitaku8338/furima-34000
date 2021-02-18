eb439b9da81f96d751cc4bf80d0dd470e065646f
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-18 12:22:00 +0900

    ログイン状態の出品者であっても、URLを直接入力して自身の売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移(商品情報編集機能)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -63,6 +63,6 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
+    redirect_to root_path if current_user != @item.user || @item.order.present?  # ログイン状態の出品者であっても、URLを直接入力して自身の売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移する
   end
 end
